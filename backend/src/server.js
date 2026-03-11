8dbdb2731c0b73aab41701a058506a5cdd2f0d95
John Aricsson Majumder <johnaricsson1@gmail.com>
2026-03-11 10:44:40 +0600

    auth & Cloudinary setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,14 +6,16 @@ import express from "express"; //type module in server.js
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
   console.log("server is running on port: " + PORT);
